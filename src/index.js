6590991ee27e6329f62e21dfd0e0eec529d80cb5
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-09-04 11:10:44 -0400

    Aliciea

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
-
-  let imageId = 1 //Enter the id from the fetched image here
+//const
+  let imageId = 3350  //Enter your assigned imageId here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  //selected image from img src tag
+  const myImage = document.querySelector("#image")
+//selected image name from h4
+  const myImageName = document.querySelector('name')  
+  //selected likes from span 
+  const likeImage = document.querySelector("likes")  
+  //selected button from button tag
+  const like_button = document.querySelector("like_button")  
+  
+  
+
+
+
+//pillar 1, fetch
+
+fetchImage()
+
+function fetchImage() {
+fetch(`${imageURL}`)
+.then(resp => resp.json())
+// .then(data => console.log("hi",data))
+.then(data => slapImageOnDom(data))
+}
+
+function slapImageOnDom(data) {
+  // console.log("hello", data.url)
+  // console.log("hello", data)
+  data.url.innerHTML += `
+  <img src=${url} />
+  `
+  // console.log(data)
+  // debugger
+ }
+
+
+//add/update
+// fetch(`${imageURL}`,{
+// method: 'PATCH',
+// headers: {
+// 'Content-Type': 'application/json',
+// 'Accept': 'application/json'
+// },
+// body: JSON.stringify({
+
+// "name": newImageName
+
+// })
+// })
+// .then(res => res.json())
+// .then(response => console.log(response))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 })
