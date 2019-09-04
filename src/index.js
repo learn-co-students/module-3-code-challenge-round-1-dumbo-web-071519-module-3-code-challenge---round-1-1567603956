5bd743c5840274fda950bfe855b2747b32128ba4
Janet Mendez <janetmendez.03@gmail.com>
2019-09-04 10:55:46 -0400

    Done with comment and refactoring

--- src/index.js
+++ src/index.js
@@ -16,25 +16,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const deleteURL = `https://randopic.herokuapp.com/comments/`
 
-
+  // This is the first Get request to our server
+  // Once it's OK-ed, we run out setUpImage()
   fetch(imageURL).then(resp => resp.json()).then(setUpImage)
 
+
   function setUpImage(imageobj) {
+    // Here we set the imageElement's src to be the obj url
     imageElement.src = imageobj.url
-    imageElement.dataset.id = imageId
+    // Here we change the name of the h4 element
     nameElement.innerText = imageobj.name
-    //console.log(imageobj)
+    // And here we set up out like count
     likes.innerText = imageobj.like_count
+    // Since the key: comments is an array, we iterate over each object in it's array....
     imageobj.comments.forEach((comm) => {
-      commentList.innerHTML += `<li>${comm.content}<button data-id="${comm.id}">X</button></li>`
+      // And then apend it with a delete button && the comment id attatchd 
+      commentList.innerHTML += `<li>${comm.content}<button data-id="${comm.id}">🗑</button></li>`
     })
   }
 
+  //This is for when the like button is clicked
   likeButton.addEventListener('click', (e) => {
     e.preventDefault()
-    let currrentLikes = parseInt(likes.innerText)
-    currrentLikes += 1
-    likes.innerText = currrentLikes
+    // Here we take our previous like count, which is on the DOM and +1 to it
+    const updatedLikes = parseInt(likes.innerText) + 1
+    // OPT REND: update the DOM first....
+    likes.innerText = updatedLikes
 
     const config = {
       method: 'POST',
@@ -44,16 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
       },
       body: JSON.stringify({
         "image_id": imageId,
-        "like_count": currrentLikes
+        "like_count": updatedLikes
       })
     }
-
+    // Then send a post request to the API
     fetch(likeURL, config)
-    //.then(resp => resp.json()).then(console.log)
+
   })
 
+  // This is for when we submit our comment form
   commentForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    // We take in the input comment value and save that...
     const comment = e.target.comment.value
     // !This was for optomistic rendering but changed to add delete button
     //commentList.innerHTML += `<li>${comment} <button>X</button></li>`
@@ -69,26 +78,31 @@ document.addEventListener('DOMContentLoaded', () => {
         "content" : comment
       })
     }
-
+    //Then we make the POST request
     fetch(commentsURL, config).then(resp => resp.json()).then(data => {
-      commentList.innerHTML += `<li>${comment} <button data-id="${data.id}">X</button></li>`
+      //This is PES REND: because we update the DOM after we get a response from the API
+      commentList.innerHTML += `<li>${comment} <button data-id="${data.id}">️🗑</button></li>`
     })
-
+    // And reset the form just to make it nice 
     e.target.reset()
   })
 
+  // This is for when there is a click in our comment list (stable parent)
   commentList.addEventListener('click', (e) => {
     e.preventDefault()
-    if(e.target.tagName === 'BUTTON'){
-      let commentId = e.target.dataset.id
-      //console.log(e.target.parentElement) //This is what i need to destroy
-
-      const config = {
-        method: 'DELETE'
-      }
-      fetch(`${deleteURL}${commentId}`, config).then(resp => resp.json()).then(() => {
+    // And if that target was a button && it has a data-id attibute
+    // It is our remove button
+    if(e.target.tagName === 'BUTTON' && e.target.dataset.id){
+      // Each button has an id that corrosponds with the comment id that we need to destroy
+      const commentId = e.target.dataset.id
+      // send a DELETE request to out API
+        fetch(`${deleteURL}${commentId}`, { method: 'DELETE' }).then(resp => resp.json()).then(() => {
+        //Once it has been sucessful, remove the comment from the DOM
         e.target.parentElement.remove()
       })
     }
+
   })
+
+
 })
